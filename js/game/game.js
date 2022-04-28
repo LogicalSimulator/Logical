14e76479a0ad49ebca1e42b4a2024db506867306
Ckyiu <38868705+UnsignedArduino@users.noreply.github.com>
2022-04-28 12:44:55 +0000

    Compress test gates together

--- js/game/game.js
+++ js/game/game.js
@@ -15,124 +15,124 @@ class Game {
 
     // testing
     console.log("creating test objects");
-    this.components.push(new Switch(createVector(50, 50)));
-    this.components.push(new Light(createVector(150, 50)));
-    this.components.push(new Light(createVector(200, 50)));
-    this.components.push(new Switch(createVector(50, 150)));
-    this.components.push(new Light(createVector(300, 100)));
-    // connect one switch to multiple lights
+    // testing switches and lights
+    this.components.push(new Switch(createVector(10, 10)));
+    this.components.push(new Light(createVector(80, 10)));
+    this.components.push(new Light(createVector(140, 10)));
+    this.components.push(new Switch(createVector(10, 70)));
+    this.components.push(new Light(createVector(200, 10)));
+    // -- connect one switch to multiple lights
     this.connections.push(make_connection(this.components[0].output1, this.components[1].input1));
     this.connections.push(make_connection(this.components[0].output1, this.components[2].input1));
-    // connect one switch to another light
+    // -- connect one switch to another light
     this.connections.push(make_connection(this.components[3].output1, this.components[4].input1));
-    // connect a switch to a light, changing it's connection
+    // -- connect a switch to a light, changing it's connection
     this.connections.push(make_connection(this.components[3].output1, this.components[2].input1));
     // buffer gate
-    this.components.push(new BufferGate(createVector(400, 50)));
-    // switch to buffer gate
-    this.components.push(new Switch(createVector(400, 200)));
-    // connect switch to buffer
+    this.components.push(new BufferGate(createVector(320, 10)));
+    // -- switch to buffer gate
+    this.components.push(new Switch(createVector(300, 80)));
+    // -- connect switch to buffer
     this.connections.push(make_connection(this.components[6].output1, this.components[5].input1));
-    // light bulb from buffer gate
-    this.components.push(new Light(createVector(600, 50)));
+    // -- light bulbs from buffer gate
+    this.components.push(new Light(createVector(400, 10)));
     this.connections.push(make_connection(this.components[5].output1, this.components[7].input1));
-    // light bulb from buffer gate
-    this.components.push(new Light(createVector(700, 50)));
+    this.components.push(new Light(createVector(450, 10)));
     this.connections.push(make_connection(this.components[5].output1, this.components[8].input1));
-    // not gate
-    this.components.push(new NotGate(createVector(500, 200)));
-    // connection from switch to not gate
+    // -- not gate
+    this.components.push(new NotGate(createVector(400, 80)));
+    // -- connection from switch to not gate
     this.connections.push(make_connection(this.components[6].output1, this.components[9].input1));
-    // light bulb from not gate
-    this.components.push(new Light(createVector(600, 200)));
+    // -- light bulb from not gate
+    this.components.push(new Light(createVector(500, 10)));
     this.connections.push(make_connection(this.components[9].output1, this.components[10].input1));
     // not gate
-    this.components.push(new NotGate(createVector(300, 300)));
-    // connect not gate to self to test
+    this.components.push(new NotGate(createVector(300, 160)));
+    // -- connect not gate to self to test
     this.connections.push(make_connection(this.components[11].output1, this.components[11].input1));
     // and gate
-    this.components.push(new AndGate(createVector(300, 400)));
-    // switches for and gate
-    this.components.push(new Switch(createVector(200, 350)));
-    this.components.push(new Switch(createVector(200, 450)));
-    // connect switches to and gate
+    this.components.push(new AndGate(createVector(300, 250)));
+    // -- switches for and gate
+    this.components.push(new Switch(createVector(200, 200)));
+    this.components.push(new Switch(createVector(200, 260)));
+    // -- connect switches to and gate
     this.connections.push(make_connection(this.components[13].output1, this.components[12].input1));
     this.connections.push(make_connection(this.components[14].output1, this.components[12].input2));
-    // connect and gate to light
-    this.components.push(new Light(createVector(400, 300)));
+    // -- connect and gate to light
+    this.components.push(new Light(createVector(380, 200)));
     this.connections.push(make_connection(this.components[12].output1, this.components[15].input1));
     // nand gate
-    this.components.push(new NandGate(createVector(500, 300)));
-    // switches for nand gate
-    this.components.push(new Switch(createVector(500, 400)));
-    this.components.push(new Switch(createVector(500, 500)));
-    // connect switches to and gate
+    this.components.push(new NandGate(createVector(500, 200)));
+    // -- switches for nand gate
+    this.components.push(new Switch(createVector(500, 260)));
+    this.components.push(new Switch(createVector(500, 320)));
+    // -- connect switches to and gate
     this.connections.push(make_connection(this.components[17].output1, this.components[16].input1));
     this.connections.push(make_connection(this.components[18].output1, this.components[16].input2));
-    // connect nand gate to lights
-    this.components.push(new Light(createVector(600, 300)));
+    // -- connect nand gate to lights
+    this.components.push(new Light(createVector(600, 200)));
     this.connections.push(make_connection(this.components[16].output1, this.components[19].input1));
-    this.components.push(new Light(createVector(650, 300)));
+    this.components.push(new Light(createVector(650, 200)));
     this.connections.push(make_connection(this.components[16].output1, this.components[20].input1));
     // or gate
-    this.components.push(new OrGate(createVector(800, 300)));
-    // switches for or gate
-    this.components.push(new Switch(createVector(700, 400)));
-    this.components.push(new Switch(createVector(700, 500)));
-    // connect switches to and gate
+    this.components.push(new OrGate(createVector(800, 260)));
+    // -- switches for or gate
+    this.components.push(new Switch(createVector(700, 200)));
+    this.components.push(new Switch(createVector(700, 260)));
+    // -- connect switches to and gate
     this.connections.push(make_connection(this.components[22].output1, this.components[21].input1));
     this.connections.push(make_connection(this.components[23].output1, this.components[21].input2));
     // conecting multiple gates together
-    this.components.push(new AndGate(createVector(400, 500)));
-    this.components.push(new OrGate(createVector(500, 600)));
-    // connect or gate output to and gate
+    this.components.push(new AndGate(createVector(400, 350)));
+    this.components.push(new OrGate(createVector(500, 400)));
+    // -- connect or gate output to and gate
     this.connections.push(make_connection(this.components[24].output1, this.components[25].input1));
-    // 3 switches, 2 for and gate and 1 for the or gate
-    this.components.push(new Switch(createVector(300, 700)));
-    this.components.push(new Switch(createVector(400, 700)));
-    this.components.push(new Switch(createVector(500, 700)));
-    // connect switches to or and and gates
+    // -- 3 switches, 2 for and gate and 1 for the or gate
+    this.components.push(new Switch(createVector(350, 480)));
+    this.components.push(new Switch(createVector(420, 480)));
+    this.components.push(new Switch(createVector(480, 480)));
+    // -- connect switches to or and and gates
     this.connections.push(make_connection(this.components[26].output1, this.components[24].input1));
     this.connections.push(make_connection(this.components[27].output1, this.components[24].input2));
     this.connections.push(make_connection(this.components[28].output1, this.components[25].input2));
-    // attach not gate to output
-    this.components.push(new NotGate(createVector(600, 600)));
-    // connect or gate output to not gate
+    // -- attach not gate to output
+    this.components.push(new NotGate(createVector(600, 450)));
+    // -- connect or gate output to not gate
     this.connections.push(make_connection(this.components[25].output1, this.components[29].input1));
-    // light bulb for output
-    this.components.push(new Light(createVector(600, 700)));
+    // -- light bulb for output
+    this.components.push(new Light(createVector(600, 350)));
     this.connections.push(make_connection(this.components[29].output1, this.components[30].input1));
     // nor gate
-    this.components.push(new NorGate(createVector(50, 300)));
-    // switches for nor gate
-    this.components.push(new Switch(createVector(50, 400)));
-    this.components.push(new Switch(createVector(50, 500)));
-    // connect switches to nor gate
+    this.components.push(new NorGate(createVector(50, 150)));
+    // -- switches for nor gate
+    this.components.push(new Switch(createVector(50, 220)));
+    this.components.push(new Switch(createVector(50, 280)));
+    // -- connect switches to nor gate
     this.connections.push(make_connection(this.components[32].output1, this.components[31].input1));
     this.connections.push(make_connection(this.components[33].output1, this.components[31].input2));
     // extended not gate to self test
-    this.components.push(new NotGate(createVector(150, 700)));
-    this.components.push(new NorGate(createVector(100, 650)));
-    this.components.push(new NotGate(createVector(50, 600)));
+    this.components.push(new NotGate(createVector(150, 450)));
+    this.components.push(new NorGate(createVector(100, 400)));
+    this.components.push(new NotGate(createVector(50, 350)));
     this.connections.push(make_connection(this.components[36].output1, this.components[35].input1));
     this.connections.push(make_connection(this.components[35].output1, this.components[34].input1));
     this.connections.push(make_connection(this.components[34].output1, this.components[36].input1));
-    this.components.push(new Switch(createVector(200, 600)));
+    this.components.push(new Switch(createVector(200, 400)));
     this.connections.push(make_connection(this.components[37].output1, this.components[35].input2));
     // xor gate
-    this.components.push(new XorGate(createVector(800, 50)));
-    // switches for xor gate
-    this.components.push(new Switch(createVector(800, 110)));
-    this.components.push(new Switch(createVector(800, 170)));
-    // connect switches to xor gate
+    this.components.push(new XorGate(createVector(750, 10)));
+    // -- switches for xor gate
+    this.components.push(new Switch(createVector(580, 10)));
+    this.components.push(new Switch(createVector(640, 10)));
+    // -- connect switches to xor gate
     this.connections.push(make_connection(this.components[39].output1, this.components[38].input1));
     this.connections.push(make_connection(this.components[40].output1, this.components[38].input2));
     // xnor gate
-    this.components.push(new XnorGate(createVector(800, 400)));
-    // switches for xnor gate
-    this.components.push(new Switch(createVector(800, 460)));
-    this.components.push(new Switch(createVector(800, 520)));
-    // connect switches to xnor gate
+    this.components.push(new XnorGate(createVector(750, 70)));
+    // -- switches for xnor gate
+    this.components.push(new Switch(createVector(580, 70)));
+    this.components.push(new Switch(createVector(640, 70)));
+    // -- connect switches to xnor gate
     this.connections.push(make_connection(this.components[42].output1, this.components[41].input1));
     this.connections.push(make_connection(this.components[43].output1, this.components[41].input2));
     console.log("done with test objects");
